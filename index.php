aaa6fff6bbde699c0fdaa361067f91441e3a1669
prasojopxl <prasojodesigner@gmail.com>
2019-10-27 22:12:49 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>..:: Web Development ::..</title>
+<meta name="description" content="Description page here"/>
+<meta name="keywords" content="Frontend, Website, HTML, CSS"/>
+<meta name="author" content="Developer"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<link rel="shortcut icon" href="assets/img/favicon.ico"/>
+<!--[if IE 8]><link rel="stylesheet" type="text/css" href="assets/css/IE8styles.css"/>
+<![endif]-->
+<script type="text/javascript" src="assets/js/jquery.js"></script>
+<script type="text/javascript" src="assets/js/myjs.js"></script>
+<style>
+	.contenthitung {
+		margin:0 auto;
+		max-width:700px;
+		padding:30px;
+		background:#ccc;
+	}
+	.contenthitung h1 {
+		margin:0;
+	}
+	.inputitem {
+		display:block;		
+		padding: 5px 0;
+	}
+	.inputitem.statik input[type="text"] {
+		background:none;
+		border:none;
+		color:#057b21;
+	}
+	.inputitem.hasil span, .inputitem.hasil input[type="text"] {
+		background:none;
+		border:none;
+		color:red;
+		font-size:20px;
+	}
+	.inputitem label {
+		display:block;
+	}
+	.hasilcbm {
+	}
+	.hasilcbm input[type="text"]{
+		width:90px;
+	}
+	
+</style>
+</head>
+<body>
+
+
+
+<div id="headerweb">
+</div><!-- endheaderweb -->
+
+<div id="bodyweb">
+<div id="footerweb">
+</div><!-- footerweb -->
+
+</body>
+</html>
