7958a28663bb1a2eb550d1c9bae5846ec85b2d50
prasojopxl <prasojodesigner@gmail.com>
2020-12-26 13:17:43 +0700

    admin lte dashboard

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,7 +1,51 @@
 $(document).ready(function () {
-    new WOW().init();
+	new WOW().init();
+	
+	// button switch dark
 	$(".wrp-switchicon").click(function(){
 		$(this).toggleClass("dark")
-	})
+	});
+
+
+	
+	// data tables
+	let example = $('#example').DataTable({
+		paging: false,
+		searching: false,
+		"info":     false,
+		columnDefs: [{
+			orderable: false,
+			className: 'select-checkbox',
+			targets: 0
+		}],
+		select: {
+			style: 'os',
+			selector: 'td:first-child'
+		},
+		order: [
+			[1, 'asc']
+		]
+	});
+	example.on("click", "th.select-checkbox", function() {
+		if ($("th.select-checkbox").hasClass("selected")) {
+			example.rows().deselect();
+			$("th.select-checkbox").removeClass("selected");
+		} else {
+			example.rows().select();
+			$("th.select-checkbox").addClass("selected");
+		}
+	}).on("select deselect", function() {
+		("Some selection or deselection going on")
+		if (example.rows({
+				selected: true
+			}).count() !== example.rows().count()) {
+			$("th.select-checkbox").removeClass("selected");
+		} else {
+			$("th.select-checkbox").addClass("selected");
+		}
+	});
+	
+
+	
 });
 
