2b4643e8ce1c0245e2e72b1388080ba1dde0c15a
prasojopxl <prasojodesigner@gmail.com>
2021-01-30 18:56:57 +0700

    calendar mailbox

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -46,6 +46,57 @@ $(document).ready(function () {
 	});
 	
 
+	// table 2
+    let myTable = $('#example2').DataTable({
+        columnDefs: [{
+            orderable: false,
+            className: 'select-checkbox',
+            targets: 0,
+        }],
+        select: {
+            style: 'os', // 'single', 'multi', 'os', 'multi+shift'
+            selector: 'td:first-child',
+		},
+		"ordering": false
+
+    });
+
+    $('#MyTableCheckAllButton').click(function() {
+		alert(",m")
+        if (myTable.rows({
+                selected: true
+            }).count() > 0) {
+            myTable.rows().deselect();
+            return;
+        }
+
+        myTable.rows().select();
+    });
+
+    myTable.on('select deselect', function(e, dt, type, indexes) {
+        if (type === 'row') {
+            // We may use dt instead of myTable to have the freshest data.
+            if (dt.rows().count() === dt.rows({
+                    selected: true
+                }).count()) {
+                // Deselect all items button.
+                $('#MyTableCheckAllButton i').attr('class', 'far fa-check-square');
+                return;
+            }
+
+            if (dt.rows({
+                    selected: true
+                }).count() === 0) {
+                // Select all items button.
+                $('#MyTableCheckAllButton i').attr('class', 'far fa-square');
+                return;
+            }
+
+            // Deselect some items button.
+            $('#MyTableCheckAllButton i').attr('class', 'far fa-minus-square');
+        }
+    });	
+	
 	
 });
 
